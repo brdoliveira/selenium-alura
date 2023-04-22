47d04205755e0b488435a51c269708dcfb282c6f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-22 02:05:34 -0300

    FEAT(CadastroLeilaoPage): Criando PageObject CadastroLeilaoPage

--- src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
@@ -1,18 +1,35 @@
 package br.com.alura.leilao.leiloes;
 
+import br.com.alura.leilao.PageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class CadastroLeilaoPage {
-    private static final String URL_CADASTRO_LEILOES = "http://localhost:8080/leiloes";
-
-    private WebDriver browser;
+public class CadastroLeilaoPage extends PageObject {
+    private static final String URL_CADASTRO_LEILOES = "http://localhost:8080/leiloes/new";
 
     public CadastroLeilaoPage(WebDriver browser) {
-        this.browser = browser;
+        super(browser);
     }
 
+    public LeiloesPage cadastrarLeilao(String nome, String valorInicial, String dataAbertura) {
+        this.browser.findElement(By.id("nome")).sendKeys(nome);
+        this.browser.findElement(By.id("valorInicial")).sendKeys(valorInicial);
+        this.browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+        this.browser.findElement(By.id("button-submit")).submit();
 
-    public void fechar() {
-        this.browser.quit();
+        return new LeiloesPage(browser);
+    }
+
+    public boolean isPaginaAtual() {
+        return this.browser.getCurrentUrl().equals(URL_CADASTRO_LEILOES);
+    }
+
+    public boolean isMensagensDeValidacoesVisiveis() {
+        String pageSource = browser.getPageSource();
+
+        return pageSource.contains("minimo 3 caracteres")
+                && pageSource.contains("não deve estar em branco")
+                && pageSource.contains("deve ser um valor maior de 0.1")
+                && pageSource.contains("deve ser uma data no formato dd/MM/yyyy");
     }
 }
