2bf47fd4e0207e65294cf2e446d82d83ed146d74
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-22 02:06:53 -0300

    FEAT(LeiloesTest): Testando métodos de teste

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -2,12 +2,26 @@ package br.com.alura.leilao.leiloes;
 
 import br.com.alura.leilao.login.LoginPage;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LeiloesTest {
     private LeiloesPage paginaDeLeiloes;
+    private CadastroLeilaoPage paginaDeCadastro;
+
+    @BeforeEach
+    public void antes(){
+        LoginPage paginaDeLogin = new LoginPage();
+        paginaDeLogin.preencheFormularioDeLogin("fulano","pass");
+        this.paginaDeLeiloes = paginaDeLogin.submeterFormulario();
+        this.paginaDeCadastro = this.paginaDeLeiloes.carregarFormulario();
+    }
 
     @AfterEach
     public void depois(){
@@ -16,10 +30,22 @@ public class LeiloesTest {
 
     @Test
     public void deveriaCadastrarLeilao(){
-        LoginPage paginaDeLogin = new LoginPage();
-        paginaDeLogin.preencheFormularioDeLogin("fulano","pass");
-        this.paginaDeLeiloes = paginaDeLogin.submeterFormulario();
-        CadastroLeilaoPage paginaDeCadastro = this.paginaDeLeiloes.carregarFormulario();
+        String hoje = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia " + hoje;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome,valor,hoje);
+
+        assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome,valor,hoje));
+    }
+
+    @Test
+    public void deveriaValidarCadastroDeLeilao(){
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao("","","");
+
+        assertFalse(this.paginaDeCadastro.isPaginaAtual());
+        assertTrue(this.paginaDeLeiloes.isPaginaAtual());
+        assertTrue(this.paginaDeCadastro.isMensagensDeValidacoesVisiveis());
     }
 
 }
